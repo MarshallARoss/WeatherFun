c8f8ffe191e7530d85af75d2e647f14333664a2a
Marshall A Ross <MarshallARoss@gmail.com>
2022-09-20 15:24:05 -0700

    Readded Secrets Template

---
+++ WeatherFun/SecretsTemplate.swift
@@ -0,0 +1,16 @@
+//
+//  SecretsTemplate.swift
+//  WeatherFun
+//
+//  Created by Marshall  on 9/20/22.
+//
+
+//MARK: - API KEYS
+/*
+import Foundation
+
+enum Secrets: String {
+    case key = {Your_API_Key_Here}
+    
+}
+ */
